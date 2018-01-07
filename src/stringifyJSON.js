b6716c3782421c03b7feca484b9dc0fe1036da75
Michael <mqngo21@gmail.com>
2018-01-07 12:17:43 -0800

    Complete StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,24 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (Array.isArray(obj)) {
+    obj = obj.map(function(item) {
+      return stringifyJSON(item);
+    });
+    return '[' + obj + ']';
+  }
+  if (obj && typeof(obj) === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof(obj[key]) === 'function') {
+        continue;
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + results.join() + '}';
+  }
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  return '' + obj;
 };
